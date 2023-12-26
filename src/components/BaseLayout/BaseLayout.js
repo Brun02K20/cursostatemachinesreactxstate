5c52a023026ebfa8e119141abe51747df65ee71c
Brun02K23 <bvirinni@gmail.com>
2023-12-26 20:42:53 -0300

    aprendiendo propiedades y metodos base para manipular la mauina de estados

--- src/components/BaseLayout/BaseLayout.js
+++ src/components/BaseLayout/BaseLayout.js
@@ -6,8 +6,13 @@ const BaseLayout = () => {
     const [state, send] = useMachine(bookingMachine); // state me da toda la info de la maquina de estados en un momento determinado en el tiempo
     // send sirve para generar transiciones
 
-    console.log("mi ME: ", bookingMachine);
-    console.log("estado actual ME: ", state)
+    // console.log("mi ME: ", bookingMachine);
+    console.log("mi ME: ", state);
+
+    console.log("matches prueba 1: ", state.matches("initial"))
+    console.log("matches prueba 2: ", state.matches("tickets"))
+    console.log("can prueba 1: ", state.can("CANCEL"))
+    console.log("jugando: ", state.machine.events)
     return (
         <div>BaseLayout</div>
     )
